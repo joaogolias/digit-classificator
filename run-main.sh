38b9b4b895ca1f63e8a2a65b859ee3c6048c20bd
Golias <joao.victor@mobile2you.com.br>
2019-05-22 20:30:06 -0300

    updated scripts to run more than 1 main at same time

--- run-main.sh
+++ run-main.sh
@@ -1,3 +1,9 @@
-g++ -std=c++11 main.cpp src/modules/basic/matrix/Matrix.cpp -o main.out
-./main.out
-exit 0
+./run-test.sh MainTask 100 5 &&
+./run-test.sh MainTask 100 10 &&
+./run-test.sh MainTask 100 15 &&
+./run-test.sh MainTask 1000 5 &&
+./run-test.sh MainTask 1000 10 &&
+./run-test.sh MainTask 1000 15 &&
+./run-test.sh MainTask 4000 5 &&
+./run-test.sh MainTask 4000 10 &&
+./run-test.sh MainTask 4000 15
--- run-test.sh
+++ run-test.sh
@@ -62,6 +62,7 @@ fi
 
 if [ $1 = "MainTask" ] 
 then
+    echo ndig=$2 p=$3
     g++ -std=c++11 -Wno-writable-strings src/helpers/timehelper/TimeHelper.cpp src/modules/factorization/qr/QrFactorization.cpp tests/testmanager/TestManager.cpp src/modules/systems/triangularsystemssolver/TriangularSystemsSolver.cpp src/modules/learning/Learning.cpp  src/modules/imageprocessor/ImageProcessor.cpp src/modules/basic/matrix/Matrix.cpp src/helpers/filehelper/FileHelper.cpp src/modules/factorization/alternatingLeastSquares/NonNegativeFactorization.cpp  src/tasks/mainTask/train/Train.cpp src/tasks/mainTask/main.cpp src/tasks/mainTask/percentagehit/PercentageHit.cpp -o MainTask.out
     ./MainTask.out $2 $3
 fi
